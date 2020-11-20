c1916b7e3d951436b1db3ec01c58c0a64e17c609
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-11-20 10:30:05 +0000

    aggiunto nuovo modulo routes

--- ESEMPIO_MONGO/app.js
+++ ESEMPIO_MONGO/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var moviesRouter= require('./routes/movie'); //aggiunto 
 
 var app = express();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/movie', moviesRouter); //aggiunto url di movie
 
 module.exports = app;
