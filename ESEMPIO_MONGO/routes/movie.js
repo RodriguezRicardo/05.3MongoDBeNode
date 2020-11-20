de598b255670a79e42ad2e90a4b601e545d56702
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-11-20 11:17:33 +0000

    ricerca film in base al titolo

--- ESEMPIO_MONGO/routes/movie.js
+++ ESEMPIO_MONGO/routes/movie.js
@@ -4,21 +4,21 @@ var router = express.Router();
 const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/movie_from_title/:title', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    title = req.params.title;
     const uri = 'mongodb+srv://ricardorodriguez:Rcy_kj0_p@nraboy-sample.bf8zs.mongodb.net/nraboy-sample?retryWrites=true&w=majority'
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         const collection = client.db("sample_mflix").collection("movies"); //Mi connetto alla collection movies
-
-        // perform actions on the collection object
-        collection.find().limit(10).toArray((err, result) => {
+        // eseguo una find sulla collection
+        collection.find({ 'title': `${title}` }).toArray((err, result) => {
             if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-            else console.log(result);
+            else res.send(result);
             client.close(); //Quando ho terminato la find chiudo la sessione con il db
         }); //Eseguo la query e passo una funzione di callback
 
     });
-    res.send('respond with a resource');
 });
 
 module.exports = router;
