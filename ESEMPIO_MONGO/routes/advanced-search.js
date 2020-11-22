3ce9d4486b6aecdd50d1c25a4a2ed504e6ec9227
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-11-22 12:29:23 +0000

    finito 05.3 ricerca in base a 2 parametri

--- ESEMPIO_MONGO/routes/advanced-search.js
+++ ESEMPIO_MONGO/routes/advanced-search.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const MongoClient = require('mongodb').MongoClient;
 
+//Ricerca un film in base a un attore
 router.get('/actors/:a', function (req, res, next) {
     console.log(req.params); 
     a = req.params.a;
@@ -20,4 +21,27 @@ router.get('/actors/:a', function (req, res, next) {
     });
 });
 
+//Ricerca un film in base a due parametri: durata e anno di produzione
+//Tutti i film che hanno una certa durata e sono stati girati prima del 2000
+router.get('/length_year/:length/:year', function(req, res, next){
+    console.log(req.params);
+    let numLength = parseInt(req.params.length);
+    let numYear = parseInt(req.params.year);
+
+    const uri = 'mongodb+srv://ricardorodriguez:Rcy_kj0_p@nraboy-sample.bf8zs.mongodb.net/nraboy-sample?retryWrites=true&w=majority'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    client.connect(err => {
+        const collection = client.db("sample_mflix").collection("movies"); 
+
+        // eseguo una find sulla collection. Due possibili modi:
+        //1)  {'runtime' : numLength, 'year' : {$lt : numYear}}
+        //2)  {$and : [{'runtime' : numLength} , {'year' : {$lt : numYear}}]}
+        collection.find({$and : [{'runtime' : numLength} , {'year' : {$lt : numYear}}]}).toArray((err, result) => {   
+            if (err) console.log(err.message); 
+            else res.send(result); 
+            client.close(); 
+        }); 
+    });
+});
 module.exports = router;
